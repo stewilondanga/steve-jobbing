cfdcb84fdb56fe3a7f8e8edbf26b52513fb2b3aa
Stewart ilondanga <stilondanga@gmail.com>
2018-12-15 10:58:46 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,18 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var num=0;
+window.setInterval(function(){
+  num++;
+  var classes=['','out','ready'];
+  $('#watch').removeClass();
+  $('#watch').addClass(classes[num%3])
+  $('#phone').removeClass();
+  $('#phone').addClass(classes[(num+2)%3]);
+  $('#laptop').removeClass();
+  $('#laptop').addClass(classes[(num+1)%3]);
+  $('.quote span').removeClass();
+  $('.quote span:nth-child('+ (num%3+1) +')').addClass('highlight');
+  console.log(num%3+1);
+},900)
